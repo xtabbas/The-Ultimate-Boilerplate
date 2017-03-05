e694f5bad7b28c43b737dedba55df867bfb82f5b
Touqeer <touqeerabbas49@gmail.com>
2017-03-05 13:41:11 +0500

    Fixed linters that came with updating airbnb config

--- src/components/containers/Welcome/Welcome.js
+++ src/components/containers/Welcome/Welcome.js
@@ -11,10 +11,14 @@ require('./welcome.scss');
 
 export class Welcome extends Component {
 
+  static defaultProps = {
+    modal: {}
+  };
+
   static propTypes = {
-    modal: PropTypes.object,
-    closeModal: PropTypes.func,
-    openModal: PropTypes.func
+    modal: PropTypes.obj,
+    closeModal: PropTypes.func.isRequired,
+    openModal: PropTypes.func.isRequired
   }
 
   componentWillMount() {}
